43f5ff47c52507ed9895ec9ddd9a290484a3cf9c
DylanVallejo <dylan1811vallejo@gmail.com>
2026-02-25 21:52:59 -0500

    cambios sobre la rama de roxan

--- TrabajoGrupalGit/Program.cs
+++ TrabajoGrupalGit/Program.cs
@@ -11,8 +11,6 @@ namespace TrabajoGrupalGit
         static void Main(string[] args)
         {
 
-            // este es un ejemplo de una nueva aplicacion en git
-            //test de prueba 3
 
         }
     }
